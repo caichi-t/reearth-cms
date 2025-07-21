bd4232989f5e1fa3a3adc4c8b3eb5eb8c12f92e7
“jasonkarel” <jasonkareli@gmail.com>
2025-07-21 14:05:09 +0700

    fix comments and config variable

--- server/internal/app/config.go
+++ server/internal/app/config.go
@@ -75,7 +75,7 @@ type Config struct {
 	HealthCheck HealthCheckConfig `pp:",omitempty"`
 
 	// Policy Checker Configuration
-	PolicyChecker PolicyCheckerConfig `pp:",omitempty"`
+	Policy_Checker PolicyCheckerConfig `pp:",omitempty"`
 }
 
 type HealthCheckConfig struct {
@@ -84,10 +84,10 @@ type HealthCheckConfig struct {
 }
 
 type PolicyCheckerConfig struct {
-	Type     string `default:"permissive" envconfig:"REEARTH_CMS_POLICY_CHECKER_TYPE"`
-	Endpoint string `envconfig:"REEARTH_CMS_POLICY_CHECKER_ENDPOINT"`
-	Token    string `envconfig:"REEARTH_CMS_POLICY_CHECKER_TOKEN"`
-	Timeout  int    `default:"30" envconfig:"REEARTH_CMS_POLICY_CHECKER_TIMEOUT"`
+	Type     string `default:"permissive"`
+	Endpoint string
+	Token    string
+	Timeout  int `default:"30"`
 }
 
 type ServerConfig struct {
